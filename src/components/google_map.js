48e41c816c720c0824df0e6e8ee792422e0f23a2
Jaspreet Atwal <jatwal@capaxglobal.com>
2017-09-24 21:33:07 +0100

    Added addtional functionality to update google map from lecture 112.

--- src/components/google_map.js
+++ src/components/google_map.js
@@ -1,8 +1,18 @@
 import React, { Component } from "react";
 
 export default class GoogleMap extends Component {
+    // Stops React re-rendering this component. Google
+    // Maps is incharge of rendering.
+    shouldComponentUpdate() {
+        return false;
+    }
+
+    componentWillReceiveProps(nextProps) {
+        this.map.panTo({ lat: nextProps.lat, lng: nextProps.lon });
+    }
+
     componentDidMount() {
-        new google.maps.Map(this.refs.map, {
+        this.map = new google.maps.Map(this.refs.map, {
             zoom: 12,
             center: {
                 lat: this.props.lat,
